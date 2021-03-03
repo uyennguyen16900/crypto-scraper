9f32f66ce97816139ad816c43590a37209db1ee8
uyennguyen16900 <uyennguyen16900@gmail.com>
2021-03-03 00:09:26 -0800

    added table test driven

---
+++ scraper.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+
+	"github.com/gocolly/colly"
+)
+
+// main() contains code adapted from example found in Colly's docs:
+// http://go-colly.org/docs/examples/basic/
+
+// Crypto struct
+type Crypto struct {
+	Name   string
+	Price  string
+	Symbol string
+}
+
+// GetCoins return a string slice of coins that have price under given price
+func GetCoins(price float64, coins []Crypto) []string {
+	var res []string
+	for _, coin := range coins {
+		fmt.Println("price", coin.Price[1:])
+		re := regexp.MustCompile(`/[0-9.]+/g`)
+		fmt.Println("heyy", re.FindAllString("44,5.6", -1))
+		// 	digits := reg.FindAllString(coin.Price, -1)
+		// 	coinPrice, _ := strconv.ParseFloat(digits, 64)
+		// 	fmt.Println(coinPrice)
+		// 	if coinPrice <= price {
+		// 		res = append(res, coin.Symbol)
+		// 	}
+	}
+	return res
+}
+
+// GetLimitPrice returns the limit price
+func GetLimitPrice() float64 {
+	fmt.Println("Enter the limit price: ")
+
+	var price float64
+	fmt.Scanln(&price)
+	return price
+}
+
+func main() {
+	fName := "cryptocoinmarketcap.csv"
+	file, err := os.Create(fName)
+	if err != nil {
+		log.Fatalf("Cannot create file %q: %s\n", fName, err)
+		return
+	}
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Write CSV header
+	writer.Write([]string{"Name", "Symbol", "Price (USD)"})
+
+	// Instantiate default collector
+	c := colly.NewCollector()
+
+	var coins []Crypto
+	// var jsonText = []byte(`[
+	//     {"Name": "", "Symbol": "", "Price": ""}
+	// ]`)
+
+	c.OnHTML("tbody", func(e *colly.HTMLElement) {
+		e.ForEach("tr", func(_ int, el *colly.HTMLElement) {
+			crypto := Crypto{
+				Name:   el.ChildText("td:nth-child(2)"),
+				Symbol: el.ChildText("td:nth-child(3)"),
+				Price:  el.ChildText(".price___3rj7O"),
+			}
+
+			writer.Write([]string{
+				crypto.Name,
+				crypto.Symbol,
+				crypto.Price,
+			})
+
+			// if err := json.Unmarshal([]byte(jsonText), &coins); err != nil {
+			// 	log.Println(err)
+			// }
+			coins = append(coins, crypto)
+
+		})
+		cryptoJSON, _ := json.MarshalIndent(coins, "", "")
+		_ = ioutil.WriteFile("output.json", cryptoJSON, 0644)
+
+		fmt.Println(GetCoins(GetLimitPrice(), coins))
+
+	})
+
+	c.Visit("https://coinmarketcap.com/all/views/all/")
+
+	log.Printf("Scraping finished, check file %q for results\n", fName)
+}
---
+++ scraper_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestGetCoins(t *testing.T) {
+	coins := []Crypto{
+		{"WashHigh", "$51.342", "GWHS"},
+		{"Marina", "$5.6", "MA"},
+		{"Polkadot", "$37.68", "DOT"},
+		{"XRP", "$0.4449", "XRP"},
+		{"Litecoin", "$193.66", "LTC"},
+		{"Chainlink", "$30.58", "LINK"},
+	}
+
+	expect := []string{"MA", "DOT", "XRP", "LINK"}
+	output := GetCoins(51.1, coins)
+
+	if len(expect) != len(output) {
+		t.Error("Test failed! Expected: {}, received: {}", expect, output)
+	}
+	for i, v := range expect {
+		if v != output[i] {
+			t.Error("Test failed! Expected: {}, received: {}", expect, output)
+		}
+	}
+}
+
+func TestGetLimitPrice(t *testing.T) {
+	output := GetLimitPrice()
+	if output != 2.18 {
+		t.Error("Test Failed: {}")
+	}
+}
+
+func BenchMarkGetCoins(b *testing.B) {
+	coins := []Crypto{
+		{"Polkadot", "$37.68", "DOT"},
+		{"XRP", "$0.4449", "XRP"},
+		{"Litecoin", "$193.66", "LTC"},
+		{"Chainlink", "$30.58", "LINK"},
+	}
+	for i := 0; i < b.N; i++ {
+		GetCoins(20.87, coins)
+	}
+}
